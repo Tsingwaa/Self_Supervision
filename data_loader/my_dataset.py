67e9cb5fa77f7adfc39fdece773efb5a5e437256
Tsingwaa <zengchh3@gmail.com>
2020-02-07 21:53:59 +0800

    Add my_dataset.py

---
+++ data_loader/my_dataset.py
@@ -0,0 +1,34 @@
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms as T
+
+
+class MyCustomDataset(Dataset):
+    def __init__(self, transforms=None):
+        # stuff
+        ...
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        # stuff
+        ...
+        # 一些读取的数据
+        img = '?'
+        if self.transforms is not None:
+            img = self.transforms(img)
+        label = 1
+        # 如果 transform 不为 None，则进行 transform 操作
+        return (img, label)
+
+    def __len__(self):
+        return len(self)
+
+
+if __name__ == "__main__":
+    # 定义我们的 transforms (1)
+    transformations = T.Compose([
+        T.CenterCrop(100),
+        T.ToTensor()
+    ])
+    # 创建 dataset
+    custom_dataset = MyCustomDataset(..., transformations)
