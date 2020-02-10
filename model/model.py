ffb8460de2c598f0e7ec8415c394bb543540c8a1
Tsingwaa <zengchh3@gmail.com>
2020-02-11 00:28:58 +0800

    update model.py

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@
 * @Author: Tsingwaa Tsang
 * @Date: 2020-02-06 15:09:19
 * @LastEditors: Tsingwaa Tsang
-* @LastEditTime: 2020-02-11 00:01:07
+* @LastEditTime: 2020-02-11 00:17:42
 * @Description: Null
 """
 
@@ -38,8 +38,30 @@ my_resnet = deepcopy(resnet18(True))
 
 class Net(nn.Module):
     def __init__(self, model, num_classes, aug_classes):
+        """
+        * @Description: 
+        * @Args: 
+        * @Return: 
+        """
         super(Net, self).__init__()
         # 先去除最后一层fc层
         self.resnet_layer = nn.Sequential(*list(model.children())[:-1])
         # 再定义四类的fc层
         self.fc1 = nn.Linear(model.fc.in_features, aug_classes)
+        self.fc2 = nn.Linear(model.fc.in_features, aug_classes)
+        self.fc3 = nn.Linear(model.fc.in_features, aug_classes)
+        self.fc4 = nn.Linear(model.fc.in_features, aug_classes)
+        self.fc5 = nn.Linear(model.fc.in_features, aug_classes)
+        self.fc6 = nn.Linear(model.fc.in_features, aug_classes)
+        self.fc7 = nn.Linear(model.fc.in_features, aug_classes)
+        self.fc8 = nn.Linear(model.fc.in_features, aug_classes)
+
+    def foward(self, x, label):
+        fc_dict = {
+            1: self.fc1, 2: self.fc2, 3: self.fc3, 4: self.fc4,
+            5: self.fc5, 6: self.fc6, 7: self.fc7, 8: self.fc8
+        }
+        output_x = self.resnet_layer(x)
+        output_x = fc_dict[label](output_x)
+
+        return output_x
