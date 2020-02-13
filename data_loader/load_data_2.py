f99180e187a36a7bf982bc85824a3ed2a895e462
Tsingwaa <zengchh3@gmail.com>
2020-02-14 00:58:15 +0800

    create load_data.py

---
+++ data_loader/load_data_2.py
@@ -0,0 +1,40 @@
+"""
+@Author: Tsingwaa Tsang
+@Date: 2020-02-14 00:56:47
+@LastEditors: Tsingwaa Tsang
+@LastEditTime: 2020-02-14 00:57:13
+@Description: Null
+"""
+
+import numpy as np
+import struct
+import os
+
+
+def load_mnist_train(path, kind='train'):
+    labels_path = os.path.join(path, '%s-labels.idx1-ubyte' % kind)
+    images_path = os.path.join(path, '%s-images.idx3-ubyte' % kind)
+    with open(labels_path, 'rb') as lbpath:
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
+    with open(images_path, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
+        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
+    return images, labels
+
+
+def load_mnist_test(path, kind='t10k'):
+    labels_path = os.path.join(path, '%s-labels.idx1-ubyte' % kind)
+    images_path = os.path.join(path, '%s-images.idx3-ubyte' % kind)
+    with open(labels_path, 'rb') as lbpath:
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
+    with open(images_path, 'rb') as imgpath:
+        magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
+        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
+    return images, labels
+
+
+path = '绝对路径'
+train_images, train_labels = load_mnist_train(path)
+test_images, test_labels = load_mnist_test(path)
