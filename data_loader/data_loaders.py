bbba3af175532d4c94e8de399daf64d98df53bc3
Tsingwaa <zengchh3@gmail.com>
2020-02-16 21:43:36 +0800

    create dataloader.py

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,15 @@
-from torchvision import datasets, transforms
+"""
+@Author: Tsingwaa Tsang
+@Date: 2020-02-06 15:09:19
+@LastEditors: Tsingwaa Tsang
+@LastEditTime: 2020-02-16 21:23:17
+@Description: Null
+"""
+
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms as T
 from base import BaseDataLoader
+from my_dataset import MnistDataset
 
 
 class MnistDataLoader(BaseDataLoader):
@@ -8,11 +18,25 @@ class MnistDataLoader(BaseDataLoader):
     """
 
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+        trsfm = T.Compose([
+            T.ToTensor(),
+            T.Normalize((0.1307,), (0.3081,))
         ])
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(
             self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+trfms = T.Compose([
+    T.Resize(224, 224),
+    T.ToTensor(),
+    T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+root = './data'
+# 创建 dataset
+train_dataset = MnistDataset(root, transforms=trfms, stage='train')
+test_dataset = MnistDataset(root, transforms=trfms, stage='test')
+
+train_dataloader = DataLoader(train_dataset, num_workers=4, batch_size=16)
+test_dataloader = DataLoader(test_dataset, num_workers=4, batch_size=1)
--- data_loader/my_dataset.py
+++ data_loader/my_dataset.py
@@ -2,42 +2,78 @@
 @Author: Tsingwaa Tsang
 @Date: 2020-02-07 21:53:29
 @LastEditors: Tsingwaa Tsang
-@LastEditTime: 2020-02-13 23:10:56
+@LastEditTime: 2020-02-16 20:33:38
 @Description: Null
 """
+
 
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from torchvision import transforms as T
+from glob import glob
+import numpy as np
+import torch
 from PIL import Image
 
 
-class MyCustomMnist(Dataset):
-    def __init__(self, root, transforms=None):
+class MnistDataset(Dataset):
+    def __init__(self, root, transforms=None, stage="train"):
         # stuff
-
-        self.transforms = transforms
+        self._root = root
+        self._transforms = transforms
+        self._stage = stage
+        self._tr_images = np.load(glob(root + r"*_train_images.npy"))
+        self._tr_labels = np.load(glob(root + r"*_train_labels.npy"))
+        self._ts_images = np.load(glob(root + r"*_test_images.npy"))
+        self._ts_labels = np.load(glob(root + r"*_test_labels.npy"))
 
     def __getitem__(self, index):
-        # stuff
-        ...
-        # 一些读取的数据
-        img = '?'
-        if self.transforms is not None:
-            img = self.transforms(img)
-        label = 1
+        out_image = []
+        out_label = []
+
+        if self._stage == 'train':
+            out_image = self._tr_images[index]
+            out_label = self._tr_labels[index]
+            if self._transforms is not None:
+                out_image = self._transforms(out_image)
+
+        elif self._stage == 'test':
+            img = self._ts_images[index]
+            label = self._ts_labels[index]
+            if self._transforms is not None:
+                img = self._transforms(img)
+            # 先将原图及其标签输入
+            out_image.append(img)
+            # 考虑最终测试时，是否以大类准确为标准，当前以旋转为具体要求
+            out_label.append(label)
+            # out_label.append(4*label)
+
+            for rot_times in [1, 2, 3]:
+                rot_img = np.rot90(img, k=rot_times)
+
+                if self._transforms is not None:
+                    rot_img = self._transforms(rot_img)
+
+                out_image.append(rot_img)
+                # 考虑最终测试时，是否以大类准确为标准
+                out_label.append(self._tr_labels[index] * 4 + rot_times)
+                # out_label.append(self._labels[index] * 4 + rot_times)
+
         # 如果 transform 不为 None，则进行 transform 操作
-        return (img, label)
+        return (out_image, out_label)
 
     def __len__(self):
-        return len(self)
+        return len(self._tr_labels)
 
 
 if __name__ == "__main__":
     # 定义我们的 transforms (1)
-    transformations = T.Compose([
-        T.CenterCrop(100),
-        T.ToTensor()
+    Ts = T.Compose([
+        T.Resize(224, 224),
+        T.ToTensor(),
+        T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
+    root = './data'
     # 创建 dataset
-    custom_dataset = MyCustomDataset(..., transformations)
+    train_dataset = MnistDataset(root, transforms=Ts, stage='train')
+    test_dataset = MnistDataset(root, transforms=Ts, stage='test')
