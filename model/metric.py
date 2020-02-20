c7108e267d27ed21471d0cec12a30ce11673da60
Tsingwaa <zengchh3@gmail.com>
2020-02-20 22:08:06 +0800

    update metric.py

--- model/metric.py
+++ model/metric.py
@@ -24,29 +24,30 @@ def conf_matrix(pred, target):
     pred = pred.tolist()
     target = target.tolist()
     diff_cls = len(set(target))
-    conf_mat = np.zeros((diff_cls, diff_cls)
+    conf_mat = np.zeros((diff_cls, diff_cls))
 
     for idx, pred_lb in enumerate(pred):
         tgt_lb = target[idx]
-
         conf_mat[pred_lb][tgt_lb] += 1
 
     return conf_mat
 
+
 def cal_mean_recall(conf_mat):
     import numpy as np
 
-    class_correct_nums=np.diag(conf_mat)
-    class_all_nums=np.sum(conf_mat, axis=1).transpose()
-    class_recall=np.around(class_correct_nums / class_all_nums, decimals=2)
+    class_correct_nums = np.diag(conf_mat)
+    class_all_nums = np.sum(conf_mat, axis=1).transpose()
+    class_recall = np.around(class_correct_nums / class_all_nums, decimals=2)
 
     return class_recall
 
+
 def my_metric2(output, target, k=3):
     with torch.no_grad():
-        pred=torch.topk(output, k, dim=1)[1]
+        pred = torch.topk(output, k, dim=1)[1]
         assert pred.shape[0] == len(target)
-        correct=0
+        correct = 0
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
