7232d248c361b8df8105dfc98d165ce5bb2fb1c3
Tsingwaa <zengchh3@gmail.com>
2020-02-24 23:02:00 +0800

    update my_dataset.py

--- data_loader/my_dataloader.py
+++ data_loader/my_dataloader.py
@@ -11,14 +11,16 @@ from torchvision import transforms as t
 
 from data_loader.my_dataset import MnistDataset
 
-trfms = t.Compose([
+transfms = t.Compose([
     t.ToTensor(),
     t.Normalize((0.1307,), (0.3081,)),
 ])
 root = './data/'
 # 创建 dataset
-train_dataset = MnistDataset(root, transforms=trfms, stage='train')
-test_dataset = MnistDataset(root, transforms=trfms, stage='test')
+train_dataset = MnistDataset(root, transforms=transfms, stage='train')
+valid_dataset = MnistDataset(root, transforms=transfms, stage='valid')
+test_dataset = MnistDataset(root, transforms=transfms, stage='test')
 
 train_dataloader = DataLoader(train_dataset, num_workers=4, batch_size=16)
-test_dataloader = DataLoader(test_dataset, num_workers=4, batch_size=1)
+valid_dataloader = DataLoader(valid_dataset, num_workers=4, batch_size=16)
+test_dataloader = DataLoader(test_dataset, num_workers=4, batch_size=16)
